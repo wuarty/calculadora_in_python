f811d44828f806e0805fb5d081ef40a954ff6746
wuarty <leonbrasilino@outlook.pt>
2025-02-20 14:20:32 -0300

    Atualizando o codigo

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ while True:
     numero_1 = input("Digite um número: ")
     numero_2 = input("Digite um número: ")
     operador = input(
-        'Digite o opeador (Adição(+), Subtraçao(-), Multipllicação(*) e Divisão(/))')
+        'Digite o opeador (Adição(+), Subtraçao(-), Multipllicação(*) e Divisão(/)): ')
 
     numeros_validos = None
 
     try:
         num_1_float = float(numero_1)
-        num_2-float = float(numero_2)
+        num_2_float = float(numero_2)
         numeros_validos = True
 
     except Exception as error:
@@ -19,7 +19,30 @@ while True:
         print('Um ou ambos dos números digitados são invalidos. ')
         continue
 
-    sair = input('Quer sair? [s]im').lower().startswith('s')
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Digite apenas um operador valido')
+        continue
+
+    if len(operador) > 1:
+        print('Operador inválido ')
+
+    print('Realizando calculo...')
+    if operador == '+':
+        resultado = num_1_float + num_2_float
+        print(f'O resultado da soma é: {resultado}')
+    elif operador == '-':
+        resultado = num_1_float - num_2_float
+        print(f'O resultado da subtração é: {resultado}')
+    elif operador == '*':
+        resultado = num_1_float * num_2_float
+        print(f'O resultado da multiplicação é: {resultado}')
+    elif operador == '/':
+        resultado = num_1_float / num_2_float
+        print(f'O resultado da divisão é: {resultado}')
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
     if sair is True:
         break
