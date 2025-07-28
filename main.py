de6f83d1c6f70bc0d58349217e92ef631cd21953
Wuarty <leonbrasilino@outlook.pt>
2025-07-28 11:16:26 -0300

    atualizando

--- main.py
+++ main.py
@@ -1,61 +1,11 @@
+
+from curses import window
+import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from main_window import MainWindow
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(sys.argv)
     main_window = QMainWindow()
-    main_window.setWindowTitle("Calculadora")
-    central_widget = QWidget()
-    main_window.setCentralWidget(central_widget)
     main_window.show()
     app.exec()
-
-# while True:
-
-#     numero_1 = input("Digite um número: ")
-#     numero_2 = input("Digite um número: ")
-#     operador = input(
-#         'Digite o opeador (Adição(+), Subtraçao(-), Multipllicação(*) e Divisão(/)): ')
-
-#     numeros_validos = False
-#     num_1_float = 0.0
-#     num_2_float = 0.0
-
-#     try:
-#         num_1_float = float(numero_1)
-#         num_2_float = float(numero_2)
-#         numeros_validos = True
-
-#     except Exception as error:
-#         numeros_validos = None
-
-#     if numeros_validos is None:
-#         print('Um ou ambos dos números digitados são invalidos. ')
-#         continue
-
-#     operadores_permitidos = '+-/*'
-
-#     if operador not in operadores_permitidos:
-#         print('Digite apenas um operador valido')
-#         continue
-
-#     if len(operador) > 1:
-#         print('Operador inválido ')
-
-#     print('Realizando calculo...')
-#     if operador == '+':
-#         resultado = num_1_float + num_2_float
-#         print(f'O resultado da soma é: {resultado}')
-#     elif operador == '-':
-#         resultado = num_1_float - num_2_float
-#         print(f'O resultado da subtração é: {resultado}')
-#     elif operador == '*':
-#         resultado = num_1_float * num_2_float
-#         print(f'O resultado da multiplicação é: {resultado}')
-#     elif operador == '/':
-#         resultado = num_1_float / num_2_float
-#         print(f'O resultado da divisão é: {resultado}')
-
-#     sair = input('Quer sair? [s]im: ').lower().startswith('s')
-
-#     if sair is True:
-    # break
---
+++ main_window.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QMainWindow, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.setWindowTitle("Calculadora")
+        self.setGeometry(100, 100, 300, 400)
+        self.show()
